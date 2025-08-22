43bdd7fbb6fafdb12874e971d134e971e0436fd0
Allisson Gonçalves da Silva <allissondasilva29@gmail.com>
2025-08-22 13:45:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'SAEB',
+    'O que é SAEB?',
+    'Sistema de Avaliação da Educação Básica'
+)
+
+criaCartao(
+    'SAEB',
+    'Qual é o principal objetivo do SAEB?',
+    'Fornecer um diagnóstico da educação básica brasileira para ajudar na melhoria do ensino.'
+)
+
+criaCartao(
+    'SAEB',
+    'De quanto em quanto tempo o SAEB é aplicado?',
+    'Bienalmente (a cada dois anos)'
+)
+
+criaCartao(
+    'SAEB',
+    'Qual órgão é responsável pela aplicação e divulgação dos resultados do SAEB?',
+    'Instituto Nacional de Estudos e Pesquisas Educacionais Anísio Teixeira (INEP)'
+)
+
+criaCartao(
+    'SAEB',
+    'Quais são as duas áreas do conhecimento que o SAEB avalia nas provas?',
+    'Língua Portuguesa e Matemática'
+)
+
+criaCartao(
+    'SAEB',
+    'Qual a relação entre o SAEB e o IDEB (Índice de Desenvolvimento da Educação Básica)?',
+    'O IDEB é um indicador que usa os resultados do SAEB e a taxa de aprovação escolar para medir a qualidade da educação.'
+)
+
+criaCartao(
+    'SAEB',
+    'Em quais etapas da educação básica o SAEB é aplicado de forma censitária?',
+    '2º, 5º e 9º anos do Ensino Fundamental e 3ª série do Ensino Médio.'
+)
+
+criaCartao(
+    'SAEB',
+    'Qual a meta para o SAEB 2025 9º ANo',
+    '5,9'
+)
+
+criaCartao(
+    'SAEB',
+    'Qual a meta para o SAEB 2025 3ª Série',
+    '5,1'
+)
+
+
+
+
+
+
